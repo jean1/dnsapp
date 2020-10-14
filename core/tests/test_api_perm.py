c6da36928ce208d4f2b5972d445a402d45f40909
jean <jean@unistra.fr>
2020-10-14 09:59:59 +0200

    Create a "tests" directory

--- core/tests/test_api_perm.py
+++ core/tests/test_api_perm.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import status
 from rest_framework.test import APITestCase
-from core.models import Namespace, Zone, Rr
+from core.models import Namespace, Zone, Rr, Zonerule
 from guardian.shortcuts import assign_perm
 
 class APIPermTests(APITestCase):
@@ -38,50 +38,64 @@ class APIPermTests(APITestCase):
         # (in 'other' namespace whose access is forbidden)
         assign_perm('access_zone', g, z3)
 
+        # Set rules for create
+        tpat1 = '^(A|AAAA|CNAME|MX)$'
+        npat1='^[-a-zA-Z0-9_.]+$'
+        r1 = Zonerule.objects.create(zone=z1,typepat=tpat1,namepat=npat1)
+        
         # Login 
         self.client.login(username='toto', password='toto')
             
     def test_001_api_namespace_get_perm_ok(self):
-        """ test access permission to zone
+        """ test access permission to an allowed namespace "default" ; should be ok
         """
         # request
-        url = '/namespace/1/'
+        
+        n1 = Namespace.objects.get(name="default")
+        url = f'/namespace/{str(n1.id)}/'
         # check
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_002_api_namespace_get_perm_denied(self):
-        """ test access permission to zone
+        """ test access to a forbidden namespace /namespace/2; should be denied
         """
         # request
-        url = '/namespace/2/'
+        n2 = Namespace.objects.get(name="other")
+        url = f'/namespace/{str(n2.id)}/'
         # check
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
     def test_003_api_zone_get_perm_ok(self):
-        """ test access permission to zone
+        """ test access permission to an allowed zone "example.com" ; should be ok
         """
+        id=str(Zone.objects.get(name="example.com").id)
         # request
-        url = '/zone/1/'
+        url = f'/zone/{id}/'
         # check
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_004_api_zone_get_perm_denied(self):
+        """ test access permission to a forbidden zone /zone/2 ; should be denied
+        """
+        id=str(Zone.objects.get(name="otherexample.com").id)
         # request
-        url = '/zone/2/'
+        url = f'/zone/{id}/'
         # check
         response =  self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_api_zone_get_perm_ok_in_denied_namespace(self):
+    def test_005_api_zone_get_perm_ok_in_denied_namespace(self):
         """ test access permission 
         """
+        id=str(Zone.objects.get(name="example.net").id)
         # request
-        url = '/zone/3/'
+        url = f'/zone/{id}/'
         # check
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    # FIXME: test collection
