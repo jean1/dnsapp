0f20f771abf485591d9f76a83ca8792483295b03
jean <jean@unistra.fr>
2020-10-23 09:39:43 +0200

    starting to add validators

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from core.validators import NamespaceNameValidator, ZoneNameValidator, RrNameValidator, TypeValidator
 from core.models import Namespace, Zone, Rr
 
 class NamespaceSerializer(serializers.ModelSerializer):
@@ -16,3 +17,8 @@ class RrSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rr
         fields = ['id', 'name', 'type', 'ttl', 'zone', 'soa_master', 'soa_mail', 'soa_serial', 'soa_refresh', 'soa_retry', 'soa_expire', 'soa_minttl', 'a', 'aaaa', 'cname', 'ns', 'prio', 'mx', 'ptr', 'txt', 'srv_priority', 'srv_weight', 'srv_port', 'srv_target', 'caa_flag', 'caa_tag', 'caa_value', 'dname' ]
+
+    def validate(self, attrs):
+        TypeValidator(attrs)
+        return attrs
+        
